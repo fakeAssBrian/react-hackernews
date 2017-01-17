683e4e2c5e6e6ebfa870d1850ad9e6c9a19b4f26
gyver98 <matthewchoi7@gmail.com>
2017-01-17 14:51:51 +1100

    Internal Component State

--- src/App.js
+++ src/App.js
@@ -21,18 +21,23 @@ const list = [
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list,
+    };
+  }
   render() {
-    
     return (
       <div className="App">
-      { list.map((item) => (
+      { this.state.list.map((item) => 
         <div key={item.objectID}>
           <span><a href={item.url}>{item.url}</a></span>
           <span>{item.author}</span>
           <span>{item.num_comments}</span>
           <span>{item.points}</span>
         </div>
-      ))}
+      )}
       </div>
     );
   }
