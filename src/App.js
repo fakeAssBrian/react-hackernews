04ce61823d4bafc8ad0880d3ea6a4973887aeab0
gyver98 <matthewchoi7@gmail.com>
2017-01-17 16:34:02 +1100

    React form and event with higher order function

--- src/App.js
+++ src/App.js
@@ -20,19 +20,37 @@ const list = [
   },
 ];
 
+// ES5
+// function isSearched(searchTerm) {
+//   return function(item) {
+//     return !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());  
+//   }
+// }
+
+// ES6
+const isSearched = (searchTerm) => (item) =>
+  !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       list,
+      searchTerm: ''
     };
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+  onSearchChange(event) {
+    this.setState({
+      searchTerm: event.target.value
+    })
   }
   onDismiss(id) {
-    function isNotId(item) {
-      return item.objectID !== id;
-    }
-
+    // function isNotId(item) {
+    //   return item.objectID !== id;
+    // }
+    const isNotId = item => item.objectID !== id;
     const updatedList = this.state.list.filter(isNotId);
     this.setState({
       list: updatedList
@@ -41,22 +59,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      { this.state.list.map((item) => 
-        <div key={item.objectID}>
-          <span><a href={item.url}>{item.url}</a></span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-            <button
-              onClick={() => this.onDismiss(item.objectID)}
-              type="button"
-            >
-              Dismiss
-            </button>
-          </span>
-        </div>
-      )}
+        <form>
+          <input 
+            type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
+        { this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
+          <div key={item.objectID}>
+            <span><a href={item.url}>{item.title}</a></span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
       </div>
     );
   }
